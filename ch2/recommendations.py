8376baaf2c304106b70beab0684fb4245e1296d9
zhao <1142749677@qq.com>
2015-09-07 20:25:03 +0800

    ch2

--- ch2/recommendations.py
+++ ch2/recommendations.py
@@ -31,4 +31,65 @@ def sim_distance(prefs,person1,person2):
 	sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2) for item in prefs[person1] if item in prefs[person2]])
 	return 1/(1+sqrt(sum_of_squares))
 
-print sim_distance(critics,'Lisa Rose','Gene Seymour')
+# print sim_distance(critics,'Lisa Rose','Gene Seymour')
+
+def sim_pearson(prefs,p1,p2):
+	si={}
+	for item in prefs[p1]:
+		if item in prefs[p2]:
+			si[item]=1
+	n=len(si)
+	if n==0:
+		return 1
+	sum1=sum([prefs[p1][it] for it in si])
+	sum2=sum([prefs[p2][it] for it in si])
+
+	sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
+	sum2Sq=sum([pow(prefs[p2][it],2) for it in si])
+
+	pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
+
+	num=pSum-(sum1*sum2/n)
+	den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+	if den==0:
+		return 0
+
+	r=num/den
+
+	return r
+
+# print sim_pearson(critics,'Lisa Rose','Jack Matthews')
+
+def topMatches(prefs,person,n=5,similarity=sim_pearson):
+	scores=[(similarity(prefs,person,other),other) for other in prefs if other!=person]
+	scores.sort()
+	scores.reverse()
+	return scores[0:n]
+
+# print topMatches(critics,'Toby',n=3)
+
+def getRecommendation(prefs,person,similarity=sim_pearson):
+	totals={	}
+	simSums={}
+	for other in prefs:
+		if other==person:
+			continue
+		sim=similarity(prefs,person,other)
+
+		if sim<=0:
+			continue
+		for item in prefs[other]:
+			if item not in prefs[person] or prefs[person][item]==0:
+				totals.setdefault(item,0)
+				totals[item]+=prefs[other][item]*sim
+
+				simSums.setdefault(item,0)
+				simSums[item]+=sim
+	ranking=[(total/simSums[item],item) for item,total in totals.items()]
+	ranking.sort()
+	ranking.reverse()
+	return ranking
+
+print getRecommendation(critics,'Toby')
+
+
